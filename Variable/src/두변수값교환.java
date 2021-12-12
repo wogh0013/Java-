7b806735527f1d422ff7770c7089c55eb83bf084
wogh0 <wogh0@DESKTOP-EJ4H020>
2021-12-12 18:12:53 +0900

    first

---
+++ Variable/src/두변수값교환.java
@@ -0,0 +1,19 @@
+
+public class 두변수값교환 {
+	public static void main(String[] args) {
+		
+		// 두 변수의 값 교환하기
+		int x=10;
+		int y=20;
+		int temp;
+		
+		temp = x;
+		x = y;
+		y = temp;
+		
+		System.out.println(x + " " + y + " "+ temp);
+		
+		
+		
+	}
+}
